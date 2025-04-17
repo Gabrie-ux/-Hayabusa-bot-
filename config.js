78c684a1726dfcf1567cef7c076006e954ff3678
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-04-17 16:06:40 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -31,13 +31,13 @@ global.prems = ['']
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.packname = '【✧】xcrisjm❖'
-global.botname = '✦xcrisjm'
-global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ xcrisjm୭'
+global.packname = '【✧】xCRISJM❖'
+global.botname = '✦xCRISJM'
+global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ xCRISJM୭'
 global.author = '(⁎˃ᴗ˂⁎)𝐌𝐚𝐝𝐞 𝐁𝐲 Gabriel𓆪'
 global.dev = '© ⍴᥆ᥕᥱrᥱძ Gabriel'
 global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨𝐬 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
-global.namebot = 'xcrisjm'
+global.namebot = 'xCRISJM'
 global.textbot = `「 🔱 CRISJMBOT 🔱 」`
 global.vs = '2.2.0'
 global.publi = '✰𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
