774fb1b18043274044d9e83a1202af6cd3c2918c
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-04-19 09:38:04 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -23,7 +23,7 @@ let tags = {
   'logos': '「EDICION LOGOS」🍨', 
 }
 
-const vid = 'https://cdnmega.vercel.app/media/xhAlGKBI@kSakdqUKWWa9fr1sUAiP2u7id5OmP1FMBFhl1xu33AM';
+const vid = 'https://cdnmega.vercel.app/media/Il5wnBQA@ckbf26DAOa0GQVAXHJqg1XT-hf1A3uVrZr8GrppB5Z4';
 
 const defaultMenu = {
   before: `*•:•:•:•:•:•:•:•:•:•☾☼☽•:•.•:•.•:•:•:•:•:•*
