8339b1a819cad3363e04026ad1b84d219231fc63
BrayanOFC <lourdesagueda93@gmail.com>
2025-04-27 19:43:51 -0700

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['update'];
 handler.tags = ['owner'];
-handler.command = ['update'];
+handler.command = ['update', 'fix'];
 handler.rowner = true;
 
 export default handler;mport { exec } from 'child_process';
