4ff8e186d39539b0a7cd5450927ac31f4075c059
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-04-29 21:14:47 -0500

    Actualizar main-menueco.js

--- plugins/main-menueco.js
+++ plugins/main-menueco.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args, command}) => {
 let tags = {
 'rpg': 'MenuEconomia',
 }
-let img = 'https://files.catbox.moe/w3s79j.jpg'
+let img = 'https://files.catbox.moe/hcp8ui.jpg'
 
   try {
           // DEFAULT MENU
