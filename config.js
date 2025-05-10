7253ac6d778618b0b4e44c5ff2273f438aebaddb
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-05-10 16:28:20 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -39,8 +39,8 @@ global.wm = '⏤͟͟͞͞⋆⬪࣪ꥈ🍨★ ׄ ꒱ xLancelot୭'
 global.author = '(⁎˃ᴗ˂⁎)𝐌𝐚𝐝𝐞 𝐁𝐲 Gabriel𓆪'
 global.dev = '© ⍴᥆ᥕᥱrᥱძ Gabriel'
 global.espera = '✰ 𝐄𝐬𝐩𝐞𝐫𝐚 𝐔𝐧 𝐌𝐨𝐦𝐞𝐧𝐭𝐨, 𝐄𝐬𝐭𝐚𝐦𝐨𝐬 𝐄𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐒𝐮 𝐏𝐞𝐝𝐢𝐝𝐨 (✿◠‿◠)'
-global.namebot = 'xLancelot'
-global.textbot = `「 🔱 Lancelot 🔱 」`
+global.namebot = 'xKatsura'
+global.textbot = `「 🔱 katsura 🔱 」`
 global.vs = '2.2.0'
 global.publi = '✰𝐒𝐢𝐠𝐮𝐞 𝐄𝐥 𝐂𝐚𝐧𝐚𝐥👇'
 
