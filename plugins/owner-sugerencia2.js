86732b12834424a70c85788546f2a3c09ede5ddf
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-05-10 18:35:32 -0500

    Actualizar owner-sugerencia2.js

--- plugins/owner-sugerencia2.js
+++ plugins/owner-sugerencia2.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     await conn.reply(channelChatId, m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
     await conn.reply(creatorsChatId, teks, m, { mentions: conn.parseMention(teks) })
 
-    m.reply('🌠 La sugerencia se envió al Staff De Lancelot y a los creadores.')
+    m.reply('🌠 La sugerencia se envió al Staff De Katsura y a los creadores.')
 }
 handler.help = ['sugerencia2']
 handler.tags = ['owner']
