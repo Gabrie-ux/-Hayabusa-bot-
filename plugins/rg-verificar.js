ff439a839a5a21cc419d65c224c8f14af18b23f8
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-05-10 20:31:45 -0500

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -92,7 +92,7 @@ let chtxt = `👤 *𝚄𝚜𝚎𝚛* » ${m.pushName || 'Anónimo'}
 🍭 *𝙽𝚞𝚖𝚎𝚛𝚘 𝚍𝚎 𝚛𝚎𝚐𝚒𝚜𝚝𝚛𝚘* »
 ⤷ ${sn}`;
 
-    let channelID = '120363387375075395@newsletter';
+    let channelID = '120363420730325289@newsletter';
         await conn.sendMessage(channelID, {
         text: chtxt,
         contextInfo: {
